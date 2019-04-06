ad366fed03f5560fc49218f05e9c71ca6184962d
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-04-06 14:20:14 +0100

    Created a Layout component and added to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import Layout from './components/Layout/Layout';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      </div>
+      <Layout>
+        <h2>Hello World</h2>
+      </Layout>
     );
   }
 }
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+
+const layout = (props) => (
+  <Fragment>
+    <main>
+      {props.children}
+    </main>
+  </Fragment>
+);
+
+export default layout;
