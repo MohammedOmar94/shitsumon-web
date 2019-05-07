262b5879b0c9c0f2f8e2f19e3b95c1fb98a870da
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-05-07 19:16:20 +0100

    Updated the background colour of the app

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
+import classes from './Layout.module.scss';
 
 import NavBar from '../UI/NavBar/NavBar';
 
 const layout = (props) => (
   <Fragment>
     <NavBar />
-    <main>
+    <main className={classes.Layout}>
       {props.children}
     </main>
   </Fragment>
