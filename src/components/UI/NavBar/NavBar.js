2090224d3e4daaf5eaa99dec0430cdba553eab7d
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-05-21 03:02:49 +0100

    Shitsumon navbar header is now correctly written in hiragana instead of katakana

--- src/components/UI/NavBar/NavBar.js
+++ src/components/UI/NavBar/NavBar.js
@@ -5,7 +5,7 @@ import classes from "./NavBar.module.scss";
 const NavBar = props => (
   <header className={classes.NavBar}>
     <h1>
-      <p className={classes.Katakana}>シツモン</p>
+      <p className={classes.Hiragana}>しつもん</p>
       shitsumon
     </h1>
   </header>
