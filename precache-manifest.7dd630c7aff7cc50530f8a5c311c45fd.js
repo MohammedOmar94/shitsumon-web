5b720b02f1e734f8fd52c38af9c80a1f8aaf932c
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-05-23 19:23:19 +0100

    Updates

---
+++ precache-manifest.7dd630c7aff7cc50530f8a5c311c45fd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ea3610ff48070637df705fbef444e9cb",
+    "url": "/shitsumon-web/static/media/shaq_wiggle.ea3610ff.gif"
+  },
+  {
+    "revision": "d4015f6cb2bd997aa98f5504c372008c",
+    "url": "/shitsumon-web/static/media/miyamoto_reject.d4015f6c.gif"
+  },
+  {
+    "revision": "3f804e42ff24d6814a19155c08419803",
+    "url": "/shitsumon-web/static/media/miyamoto_approve.3f804e42.gif"
+  },
+  {
+    "revision": "4520646d3624c9acf78265f85b1177fe",
+    "url": "/shitsumon-web/static/media/angry_kirby.4520646d.jpg"
+  },
+  {
+    "revision": "62e3952676d408592446",
+    "url": "/shitsumon-web/static/js/runtime~main.7cbc342c.js"
+  },
+  {
+    "revision": "d6b047429c0e1bfc8a49",
+    "url": "/shitsumon-web/static/js/main.3dc8ebb0.chunk.js"
+  },
+  {
+    "revision": "e42350a0c68f73bf306d",
+    "url": "/shitsumon-web/static/js/2.dd4fc4e5.chunk.js"
+  },
+  {
+    "revision": "d6b047429c0e1bfc8a49",
+    "url": "/shitsumon-web/static/css/main.8de88d0a.chunk.css"
+  },
+  {
+    "revision": "9d25947b288af73728a935496ff669ca",
+    "url": "/shitsumon-web/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.edfa3591cd6e56b6e7dfd4087a68ee0a.js"
+  "/shitsumon-web/precache-manifest.7dd630c7aff7cc50530f8a5c311c45fd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/shitsumon-web/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
