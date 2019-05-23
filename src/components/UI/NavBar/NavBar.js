a94f6d4c9a41e4d3b9f0e9a281d565a835ace5d5
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-05-23 19:04:35 +0100

    Swapped withRouter with a Link component in Navbar

--- src/components/UI/NavBar/NavBar.js
+++ src/components/UI/NavBar/NavBar.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { withRouter } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import classes from "./NavBar.module.scss";
 
 const NavBar = props => (
   <header className={classes.NavBar}>
-    <h1 onClick={() => props.history.push('/')}>
-      <p className={classes.Hiragana}>しつもん</p>
-      shitsumon
-    </h1>
+    <Link to={{pathname: '/'}}>
+      <h1>
+        <p className={classes.Hiragana}>しつもん</p>
+        shitsumon
+      </h1>
+    </Link>
   </header>
 );
 
-export default withRouter(NavBar);
+export default NavBar;
