e52f5b8cf4c2379f864aa5c5bf66e596de440fa3
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-05-24 02:25:34 +0100

    Created files for japanese day and month constants, each including the japanese translation

---
+++ src/japanese/months.js
@@ -0,0 +1,16 @@
+const months = [
+  { month: "January", translations: [ "ichigatsu" ]},
+  { month: "Feburary", translations: [ "nigatsu" ]},
+  { month: "March", translations: [ "sangatsu" ]},
+  { month: "April", translations: [ "shigatsu" ]},
+  { month: "May", translations: [ "gogatsu" ]},
+  { month: "June", translations: [ "rokugatsu" ]},
+  { month: "July", translations: [ "shichigatsu"  ]},
+  { month: "August", translations: [ "hachigatsu" ]},
+  { month: "September", translations: [ "kugatsu" ]},
+  { month: "October", translations: [ "juugatsu" ]},
+  { month: "November", translations: [ "juuichigatsu" ]},
+  { month: "December", translations: [ "juunigatsu" ]}
+];
+
+export default months;
