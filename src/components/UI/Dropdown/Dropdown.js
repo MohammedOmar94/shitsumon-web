ade740e437636ef77bf2d691c5e2652f3bd1e0e0
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-08-24 15:20:47 +0100

    Refractored dropdown component

--- src/components/UI/Dropdown/Dropdown.js
+++ src/components/UI/Dropdown/Dropdown.js
@@ -1,17 +1,32 @@
-import React from "react";
-import PropTypes from "prop-types";
+import './styles.scss';
 
-import classes from "./Dropdown.module.scss";
+import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
-const dropdown = props => (
-  <div className={classes.DropdownContainer}>
-    <p className={classes.DropdownLabel}>{props.label}</p>
-    <select className={classes.Dropdown} onChange={(evt) => props.onChange(evt)}>
-      {props.options.map(option => (
-        option && <option value={option.value}>{option.name}</option>
-      ))}
-    </select>
-  </div>
-);
+dropdown.propTypes = {
+  className: PropTypes.string,
+  label: PropTypes.string,
+  onChange: PropTypes.func,
+  options: PropTypes.object
+}
+
+function dropdown({ className, label, onChange, options }) {
+  const dropdownClasses = classnames(
+    'dropdown',
+    className
+  )
+
+  return (
+    <div className={dropdownClasses}>
+      <p className='dropdown__label'>{label}</p>
+      <select className='dropdown__select' onChange={(evt) => onChange(evt)}>
+        {options.map(option => (
+          option && <option value={option.value}>{option.name}</option>
+        ))}
+      </select>
+    </div>
+  );
+}
 
 export default dropdown;
