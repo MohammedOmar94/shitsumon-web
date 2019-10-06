694adfcc3cccfa3566e37e8dacc43a498a26bd84
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-10-06 19:32:07 +0100

    Burger Menu also shows on mobile.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,25 +1,17 @@
 import React, { Fragment } from "react";
-import { withRouter } from 'react-router-dom';
 
 import classes from "./Layout.module.scss";
-import NavLinks from "../UI/NavLinks/NavLinks";
 import Header from "../UI/Header/Header";
 
 const layout = props => {
   document.createElement('main');
-  let navLinks = null;
-  if (props.location.pathname !== '/quiz') {
-    navLinks = <NavLinks />;
-  }
+
   return (
     <Fragment>
       <Header onBurgerClick={props.drawerClickHandler} />
       <main className={classes.Layout}>{props.children}</main>
-      <footer>
-        { navLinks }
-      </footer>
     </Fragment>
   );
 };
 
-export default withRouter(layout);
+export default layout;
