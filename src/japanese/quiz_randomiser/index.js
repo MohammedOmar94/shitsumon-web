975b991dfc476c83d9345669cc19789adc937648
Heights94 <mohammed_omar94@hotmail.co.uk>
2019-11-07 22:02:38 +0000

    Added randomised quiz for times

--- src/japanese/quiz_randomiser/index.js
+++ src/japanese/quiz_randomiser/index.js
@@ -1,11 +1,15 @@
 import { DATES_KEY, DATES_TITLE } from '../../components/Topics/constants/dates';
+import { TIMES_KEY, TIMES_TITLE } from '../../components/Topics/constants/times';
 
 import { setUpDatesQuiz } from '../dates/utils';
+import { setUpTimesQuiz } from '../times/utils';
 
 function getRandomisedQuiz(quiz, quizLength) {
   switch (quiz) {
     case DATES_KEY:
         return { questions: setUpDatesQuiz(quizLength), sectionName: DATES_TITLE };
+    case TIMES_KEY:
+        return { questions: setUpTimesQuiz(quizLength), sectionName: TIMES_TITLE };
     default:
       break;
   }
--- src/japanese/times/utils.js
+++ src/japanese/times/utils.js
@@ -1,3 +1,5 @@
+import _sample from 'lodash/sample';
+
 import jpHoursOfTheDay from  './hours_of_the_day';
 import jpMinutes from  './minutes';
 
@@ -8,7 +10,7 @@ import {
   MINUTES_UP_TO_60_PARAM,
 } from '../../components/Topics/constants/times';
 
-import { shuffle, createQuestion, getEnglishWord, getJapaneseTranslations } from '../quiz_setup';
+import { setQuizLength, shuffle, createQuestion, getEnglishWord, getJapaneseTranslations } from '../quiz_setup';
 
 function getTimesQuiz(quiz, quizLength) {
   switch (quiz) {
@@ -50,6 +52,39 @@ function setUpMinutesQuiz(quiz) {
     const timesEng = times[i].word === '1' ? `${getEnglishWord(times[i])} minute` : `${getEnglishWord(times[i])} minutes`
     const timesJp = getJapaneseTranslations(times[i]);
     questions[i] = createQuestion(i + 1, `${timesEng}`, `${timesJp}`);
+  }
+  return questions;
+}
+
+function formatHour(number) {
+  return number.split(':')[0];
+}
+
+function formatMinute(number) {
+  return number < 10 ? `0${number}` : number;
+}
+
+export function setUpTimesQuiz(quizlength) {
+  // Ignore 60th minute.
+  jpMinutes.length = 59;
+
+  const timeOfDayArr = [
+    { timeOfDayEng: "am", timeOfDayJp: "gozen" },
+    { timeOfDayEng: "pm", timeOfDayJp: "gogo" }
+  ];
+
+  // Randomise days and months
+  const hours = setQuizLength([...jpHoursOfTheDay], quizlength);
+  const minutes = setQuizLength([...jpMinutes], quizlength);
+  let questions = [];
+  for (let i = 0; i < hours.length; i++) {
+    const hoursEng = formatHour(getEnglishWord(hours[i]));
+    const minutesEng = formatMinute(getEnglishWord(minutes[i]));
+    const hoursJp = getJapaneseTranslations(hours[i]);
+    const minutesJp = getJapaneseTranslations(minutes[i]);
+    const { timeOfDayEng, timeOfDayJp } = _sample(timeOfDayArr);
+
+    questions[i] = createQuestion(i + 1, `${hoursEng}:${minutesEng}${timeOfDayEng}`, `${timeOfDayJp}${hoursJp}${minutesJp}`);
   }
   return questions;
 }
