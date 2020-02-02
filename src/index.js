5644563cba7139423a7f4a43dd6175d314fcb21f
Heights94 <mohammed_omar94@hotmail.co.uk>
2020-02-02 00:07:39 +0000

    Enabled redux dev tools

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import App from './App';
 import reducers from './reducers'
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducers)
+const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 
 const app = (
   <Provider store={store}>
